8d8355c92f59f969320bb9df808ee297d3b46243
Michael Ungar <michael.ungar03@gmail.com>
2023-01-22 16:10:03 +0100

    custom device view

--- src/device_manager/view/DevicesView.ts
+++ src/device_manager/view/DevicesView.ts
@@ -16,16 +16,20 @@ export default class DevicesView extends View {
 
   template({ ppfx, label }: { ppfx: string; label: string }) {
     return html`
-      <div class="${ppfx}device-label">${label}</div>
-      <div class="${ppfx}field ${ppfx}select">
-        <span id="${ppfx}input-holder">
-          <select class="${ppfx}devices"></select>
+      <div class="${ppfx}field ${ppfx}devices-c bg-green-300">
+        <span changeDeviceToDesktop class="${ppfx}pn-btn fa fa-desktop "></span>
+        <span changeDeviceToDeetsTerminal class="${ppfx}pn-btn ">
+          <svg width="15" height="24" viewBox="0 0 15 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M0 17.8936V8.58125C0 8.23686 0.0881144 7.89819 0.255961 7.59746L3.56173 1.67463C3.86207 1.13652 4.39177 0.765082 5.00001 0.666065L8.92751 0.0267059C9.50873 -0.067911 10.0646 0.0945823 10.4889 0.42391L10.4903 0.421164L10.5476 0.471262C10.5699 0.489891 10.5917 0.508996 10.6131 0.528561L14.305 3.75629L14.3042 3.7577C14.7278 4.1251 15 4.6676 15 5.28244V20.2933C15 21.3156 14.2358 22.1765 13.2207 22.2977L5.98747 23.1613C5.35839 23.2364 4.76893 23.0149 4.35014 22.6131L0.788414 19.4941C0.311465 19.127 0 18.5504 0 17.8936ZM4.81273 1.99586C4.59931 1.81463 4.27352 1.87342 4.1369 2.11782L1.09408 7.56097C0.990418 7.74641 1.03244 7.97929 1.19439 8.1168L3.84289 10.3657C4.05632 10.5469 4.38211 10.4882 4.51873 10.2438L7.56154 4.80061C7.66521 4.61517 7.62319 4.38229 7.46124 4.24478L4.81273 1.99586Z"
+              fill="black"
+            />
+          </svg>
         </span>
-        <div class="${ppfx}sel-arrow">
-          <div class="${ppfx}d-s-arrow"></div>
-        </div>
+        <span changeDeviceToMobile class="${ppfx}pn-btn fa fa-mobile"></span>
       </div>
-      <button style="display:none" class="${ppfx}add-trasp" data-add-trasp>+</button>
     `;
   }
 
@@ -33,6 +37,9 @@ export default class DevicesView extends View {
     return {
       change: 'updateDevice',
       'click [data-add-trasp]': 'startAdd',
+      'click [changeDeviceToDesktop]': 'changeDeviceToDesktop',
+      'click [changeDeviceToMobile]': 'changeDeviceToMobile',
+      'click [changeDeviceToDeetsTerminal]': 'changeDeviceToDeetsTerminal',
     };
   }
 
@@ -42,6 +49,36 @@ export default class DevicesView extends View {
     this.em = this.config.em;
     this.ppfx = this.config.pStylePrefix || '';
     this.listenTo(this.em, 'change:device', this.updateSelect);
+  }
+
+  changeDeviceToDesktop() {
+    console.log('lakjdfsf');
+    const { em } = this;
+
+    if (em) {
+      const devEl = this.devicesEl;
+      em.set('device', 'Desktop');
+    }
+  }
+
+  changeDeviceToMobile() {
+    console.log('lakjdfsasdf');
+    const { em } = this;
+
+    if (em) {
+      const devEl = this.devicesEl;
+      em.set('device', 'mobilePortrait');
+    }
+  }
+
+  changeDeviceToDeetsTerminal() {
+    console.log('lakjdfsf');
+    const { em } = this;
+
+    if (em) {
+      const devEl = this.devicesEl;
+      em.set('device', 'deetsTerminal');
+    }
   }
 
   /**
@@ -69,6 +106,7 @@ export default class DevicesView extends View {
    * @private
    */
   updateSelect() {
+    console.log('klasjdfl');
     const { em, devicesEl } = this;
 
     if (em && em.getDeviceModel && devicesEl) {
